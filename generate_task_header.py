691ddc3f3db0bfc326361046785394ab10f24b22
Omar Rizk <rizk.omarsherif9@gmail.com>
2024-06-19 14:05:36 +0300

    File to generate headers used for tasks

---
+++ generate_task_header.py
@@ -0,0 +1,35 @@
+import datetime
+
+def create_header(task_number, task_description, author, course, diploma):
+    date = datetime.datetime.now().strftime("%d-%m-%Y")
+    header = f"""
+\"\"\"
+Task Number: {task_number}
+Task Description: {task_description}
+
+Author: {author}
+Course: {course}
+Diploma: {diploma}
+
+Date: {date}
+\"\"\"
+"""
+    return header
+
+def main():
+    task_number = input("Enter task number: ")
+    task_description = input("Enter task description: ")
+    author = "Omar Rizk"
+    course = "Python Programming"
+    diploma = "Embedded Linux Diploma (Under Supervision of Eng. Moatasem Elsayed)"
+
+    header = create_header(task_number, task_description, author, course, diploma)
+    
+    file_name = f"task{task_number}.py"
+    with open(file_name, "w") as file:
+        file.write(header)
+
+    print(f"{file_name} has been created with the header.")
+
+if __name__ == "__main__":
+    main()
