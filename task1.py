de7754e864ec71ac8bf1a133c57a8815dd073b80
Omar Rizk <rizk.omarsherif9@gmail.com>
2024-06-19 14:21:58 +0300

    Task 1 Solution

--- task1.py
+++ task1.py
@@ -9,12 +9,23 @@ Diploma: Embedded Linux Diploma (Under Supervision of Eng. Moatasem Elsayed)
 """
 
 def is_numeric(x):
+    """
+    Checks if an input can be converted to an integer.
+
+    Args:
+    - x: The input value to check.
+
+    Returns:
+    - True if x can be converted to an integer.
+    - False otherwise.
+    """
     try:
         int(x)
         return True
     except ValueError:
         return False
-        
+
+
 # Prompt the user to enter a list of items separated by spaces
 user_input = input("Enter a list of items separated by spaces: ")
 
@@ -24,6 +35,8 @@ str_list  = user_input.split()
 # Filter out non-numeric values and convert the rest to integers
 int_list = [int(item) for item in str_list if is_numeric(item)]
 
-print("The list you entered is:", int_list)
+# Print the integer-only list
+print(f"The integer-only list is: {int_list}")
 
-print(int_list.count(4))
+# Print the count of number 4 in the integer-only list
+print(f" Number 4 appears in the list {int_list.count(4)} times!")
