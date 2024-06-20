1da5ae8d8f2ec457dc77d7f9b723ba46ceba7928
Omar Rizk <rizk.omarsherif9@gmail.com>
2024-06-20 08:52:44 +0300

    Script to generate tasks headers

--- generate_header_files.py
+++ generate_header_files.py
@@ -1,24 +1,25 @@
-def create_header(task_number, task_description, author, course, diploma):
-    header = f"""
-\"\"\"
-Task Number: {task_number}
-Task Description: {task_description}
+def create_header(task_number, task_description, task_objective, author, course, diploma):
+    header = f"""\"\"\"
+Task Number      : {task_number}
+Task Description : {task_description}
+Task Objective   : {task_objective}
 
-Author: {author}
-Course: {course}
-Diploma: {diploma}
+Author  : {author}
+Course  : {course}
+Diploma : {diploma}
 \"\"\"
 """
     return header
 
 def main():
-    task_number = input("Enter task number: ")
+    task_number      = input("Enter task number: ")
     task_description = input("Enter task description: ")
+    task_objective   = input("Enter task objective: ")
     author = "Omar Rizk"
     course = "Python Programming"
     diploma = "Embedded Linux Diploma (Under Supervision of Eng. Moatasem Elsayed)"
 
-    header = create_header(task_number, task_description, author, course, diploma)
+    header = create_header(task_number, task_description, task_objective, author, course, diploma)
     
     file_name = f"task{task_number}.py"
     with open(file_name, "w") as file:
